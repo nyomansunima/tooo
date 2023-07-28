52b0b8deb012f16fe958a330eb5cc0e1431e9164
Nyoman Sunima <nyomansunima@gmail.com>
2023-07-28 11:19:07 +0800

    🌵 style: modify the style including in the tailwindcss and globas

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,18 +1,29 @@
+import tailwindcssForm from '@tailwindcss/forms'
+import tailwindcssAspectRatio from '@tailwindcss/aspect-ratio'
+import tailwindcssTypography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: '#0C0D10',
+        brand: {
+          blue: '#1C23F6',
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssForm, tailwindcssAspectRatio, tailwindcssTypography],
 }
