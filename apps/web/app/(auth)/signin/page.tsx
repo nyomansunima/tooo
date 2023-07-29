e0bf2a08fb7258d1096b792051e069acaf42aca8
Nyoman Sunima <nyomansunima@gmail.com>
2023-07-29 15:23:08 +0800

    👛 update: add the google and github button

--- apps/web/app/(auth)/signin/page.tsx
+++ apps/web/app/(auth)/signin/page.tsx
@@ -2,10 +2,21 @@
 
 import { Button } from '@components/ui/button'
 import { useMutation } from '@tanstack/react-query'
+import { signIn } from 'next-auth/react'
+import Image from 'next/image'
 import { FC } from 'react'
 
 const SigninPage: FC = () => {
-  const signinUsingGoogle = useMutation(async () => {}, {})
+  const signinUsingGoogle = useMutation(async () => {
+    await signIn('google', {
+      redirect: false,
+    })
+  }, {})
+  const signinUsingGithub = useMutation(async () => {
+    await signIn('github', {
+      redirect: false,
+    })
+  }, {})
 
   return (
     <main className="flex h-screen w-screen">
@@ -13,10 +24,35 @@ const SigninPage: FC = () => {
         <h2 className="text-5xl font-bold leading-snug">
           Start grow your business from the best demand on market.
         </h2>
-        <Button onClick={() => signinUsingGoogle.mutate()}>
-          <i className="fi fi-brands-google" />
-          Google
-        </Button>
+
+        <div className="flex items-center gap-4 mt-16">
+          <Button
+            variant={'outline'}
+            size={'madium'}
+            className="hover:-translate-y-1"
+            onClick={() => signinUsingGoogle.mutate()}
+          >
+            <i className="fi fi-brands-google" />
+            Google
+          </Button>
+          <Button
+            variant={'outline'}
+            size={'madium'}
+            className="hover:-translate-y-1"
+            onClick={() => signinUsingGithub.mutate()}
+          >
+            <i className="fi fi-brands-github" />
+            Github
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-center items-center w-1/2 h-full bg-[#F2F3FE] dark:bg-[#1F2022]">
+        <Image
+          src={'/images/phonies_shopping_call.svg'}
+          height={460}
+          width={460}
+          alt="Signin"
+        />
       </div>
     </main>
   )
