b35dc4ff58210ad5bbca55eff46d90b2242bf98c
Nyoman Sunima <nyomansunima@gmail.com>
2023-07-29 15:26:40 +0800

    🤗 refactor: add and change the structure

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,44 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { settingConfiguration } from './config/settings'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
+import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
+import { PrismaService } from './config/prisma.service'
+import { JwtAuthGuard } from './auth/guard/jwt-auth.guard'
 
+@Global()
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [settingConfiguration],
+    }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+
+    // TODO: add the feature module
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  exports: [PrismaService],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,43 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common'
+import * as compression from 'compression'
+import helmet from 'helmet'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+const logger = new Logger('MAIN')
+
+// allow to register the swagger api
+// through the document and as the api documentation
+async function registerSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Tooo Core API')
+    .setDescription('The core api documentation')
+    .addTag('core')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const app = await NestFactory.create(AppModule)
+
+  // register all deps and settings
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.use(compression())
+  app.use(helmet())
+  app.enableCors({ origin: '*' })
+
+  const configService = app.get<ConfigService>(ConfigService)
+  const port = configService.get<number>('port')
+
+  await registerSwagger(app)
+
+  await app.listen(port, () => {
+    logger.log(`🏀 The application running on port ${port}`)
+  })
 }
-bootstrap();
+
+bootstrap()
